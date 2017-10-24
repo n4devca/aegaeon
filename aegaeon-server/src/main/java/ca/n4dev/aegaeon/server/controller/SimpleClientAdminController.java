c698e07298ba421c84614522409b0ef6857aae59
Remi Guillemette <rguillemette@n4dev.ca>
2017-10-24 07:16:14 -0400

    Basic controller and page for admin client

---
+++ aegaeon-server/src/main/java/ca/n4dev/aegaeon/server/controller/SimpleClientAdminController.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2017 Remi Guillemette - n4dev.ca
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package ca.n4dev.aegaeon.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * SimpleClientAdminController.java
+ * 
+ * TODO(rguillemette) Add description
+ *
+ * @author by rguillemette
+ * @since Oct 24, 2017
+ */
+@Controller
+@RequestMapping(value = SimpleClientAdminController.URL)
+@ConditionalOnProperty(prefix = "aegaeon.modules", name = "admin", havingValue = "true", matchIfMissing = false)
+public class SimpleClientAdminController extends BaseUiController {
+
+    public static final String URL = "/admin-clients";
+    public static final String VIEW = "admin-clients";
+    /**
+     * @param pMessages
+     */
+    @Autowired
+    public SimpleClientAdminController(MessageSource pMessages) {
+        super(pMessages);
+    }
+
+    @GetMapping("")
+    public ModelAndView index() {
+        return new ModelAndView(VIEW);
+    }
+}
